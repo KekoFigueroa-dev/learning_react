7190e1c6ccaf30910328d876f9cd65070f61594a
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-03-23 15:24:23 -0400

    second commit2.0

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "https://github.com/KekoFigueroa-dev/learning_react.git",
+    base: "/learning_react",
   plugins: [react()],
 })
